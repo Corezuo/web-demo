c2950e60db23af2e3042927d7cf172e4c87b3e5d
zfh <fuhongzuo@gmail.com>
2018-12-06 17:52:20 +0800

    1.完成右侧预览组件的显示 2.组件的联动控制

--- case4/js/main.js
+++ case4/js/main.js
@@ -19,8 +19,8 @@ myApp.controller('myCtrl', ['$scope', '$http', function ($scope, $http) {
                     if (component.component) {
                         module.components[j] = component.component;
                     }
-                    // 组件默认禁用
-                    componentEnableStaus[module.components[j].id] = false;
+                    // 默认启用
+                    componentEnableStaus[module.components[j].id] = true;
                 }
                 modules.push(module);
             }
@@ -76,13 +76,101 @@ myApp.controller('myCtrl', ['$scope', '$http', function ($scope, $http) {
 }]);
 
 /**
+ * Register style filters
+ */
+myApp.filter("filterStyle", function () {
+    return function (input) {
+        var valueStyleStr = "";
+        if ("valueStyle" in input && input.valueStyle != null && input.valueStyle !== "") {
+            var valueStyle = JSON.parse(input.valueStyle);
+            angular.forEach(valueStyle, function (value, key) {
+                valueStyleStr += humpToLine(key) + ":" + value + ";";
+            });
+        }
+        if ("width" in input && input.width != null && input.width !== "") {
+            valueStyleStr += "width:" + input.width + "%;";
+        }
+        return valueStyleStr;
+    }
+});
+
+/**
  * This controller is used to adjust the template component style
  * @param $scope
  * @param $http
  */
 myApp.controller('previewCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.isFlag = true;
-    $scope.getSystemComponent = function () {
-        $scope.isFlag = false;
-    }
+    $scope.doTemplateActive = function (templateComponent) {
+        console.log("moduleId: ", templateComponent.moduleId);
+        console.log("componentId: ", templateComponent.id);
+        if (templateComponent.valueStyle == null || templateComponent === "") {
+            templateComponent.valueStyle = {};
+        }
+
+        // Todo: 通过位置，找到指定组件。将其加边框，通过缓存，剔除上一个元素的边框。
+        var valueStyle = JSON.parse(templateComponent.valueStyle);
+        if (valueStyle.hasOwnProperty("border") && valueStyle.hasOwnProperty("backgroundColor")) {
+            delete valueStyle.border;
+            delete valueStyle.backgroundColor;
+        } else {
+            valueStyle.border = "1px solid orange";
+            valueStyle.backgroundColor = "rgba(247, 248, 74, 0.67)";
+        }
+        templateComponent.valueStyle = JSON.stringify(valueStyle);
+    };
+
+    var param = {
+        url: "data/system-component.json",
+        sCallback: function (res) {
+            var modules = res.data.modules;
+            var moduleId = 4;
+            var componentId = 559;
+
+            angular.forEach(modules, function (value, key) {
+                if (value.moduleId === moduleId) {
+                    angular.forEach(value.components, function (value, key) {
+                        // text
+                        if (!(value.hasOwnProperty("component") && value.component != null && value.component !== "")) {
+                            if (value.id === componentId) {
+                                console.log("componentName: ", value.label);
+                            }
+                        }
+                        // category
+                        if (value.hasOwnProperty("category") && value.category != null && value.category !== "") {
+                            var category = value.category;
+                            if (category.id === componentId) {
+                                console.log("componentName: ", category.label);
+                            }
+                        }
+                        // tableTitle
+                        if (value.hasOwnProperty("tableTitle") && value.tableTitle != null && value.tableTitle !== "") {
+                            var tableTitle = value.tableTitle;
+                            if (tableTitle.id === componentId) {
+                                console.log("componentName: ", tableTitle.label);
+                            }
+                        }
+                        // thead
+                        if (value.hasOwnProperty("thead") && value.thead != null && value.thead !== "") {
+                            angular.forEach(value.thead.tableRowSet.columnSet, function (value, key) {
+                                if (value.id === componentId) {
+                                    console.log("componentName", value.label);
+                                }
+                            })
+                        }
+                        // tbody
+                        if (value.hasOwnProperty("tbody") && value.tbody != null && value.tbody !== "") {
+                            angular.forEach(value.tbody.tableRowSet, function (value, key) {
+                                angular.forEach(value.columnSet, function (value, key) {
+                                    if (value.id === componentId) {
+                                        console.log("componentName: ", value.label);
+                                    }
+                                });
+                            })
+                        }
+                    });
+                }
+            });
+        }
+    };
+    baseRequest($http, param);
 }]);
--- case4/js/utils.js
+++ case4/js/utils.js
@@ -18,3 +18,23 @@ function baseRequest ($http, param) {
         }
     );
 }
+
+/**
+ * 横线连接转换驼峰
+ * @param value 横线连接
+ * @return {string}
+ */
+function lineToHump(value) {
+    return value.replace(/\-(\w)/g, function(all, letter){
+        return letter.toUpperCase();
+    });
+}
+
+/**
+ * 驼峰转换横线连接
+ * @param value 驼峰
+ * @returns {string}
+ */
+function humpToLine(value) {
+    return value.replace(/([A-Z])/g,"-$1").toLowerCase();
+}
