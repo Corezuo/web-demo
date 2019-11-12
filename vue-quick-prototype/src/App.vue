1bf3ee99df609d0082a8b2a8936fb400f209686a
zfh <fuhongzuo@gmail.com>
2019-11-13 00:13:28 +0800

    更新

---
+++ vue-quick-prototype/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import axiosInst from './utils/request'
+  export default {
+    name: 'app',
+    created: function(){
+      console.log("开启请求");
+      axiosInst.get("/test")
+      .then(function (req) {
+        console.log("请求成功！")
+        console.log(req);
+      }).catch(function(err){
+        console.log("请求失败！")
+        console.log(err);
+      });
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    width: 200px;
+    border: 1px solid red;
+  }
+</style>
