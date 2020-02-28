b004d962d1ead8d66a8b7c85fca133a7aee232e3
zfh <fuhongzuo@gmail.com>
2020-02-28 16:52:36 +0800

    commonJS 模块规范

--- syntax/commonjs/main.js
+++ syntax/commonjs/main.js
@@ -1,4 +1,8 @@
-var user = require("./utils");
+// 1.加载模块（其实是加载该模块的`module.exports`属性）
+var obj = require("./utils");
+console.log(obj.name);
+obj.getName();
 
-console.log(user.name);
-user.getName();
+// 2.加载模块，方式二
+// var utils = require('./utils')
+// console.log(utils);
--- syntax/commonjs/utils.js
+++ syntax/commonjs/utils.js
@@ -1,5 +1,9 @@
-// 对外输出
+// 1.对外输出
 module.exports.name = "dazuo";
 module.exports.getName = function () {
     console.log("this method is getName");
 }
+
+// 2.方式二，导出
+// let Utils = "dazuo"
+// module.exports = Utils
