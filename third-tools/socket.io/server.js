c471f3bfe30459dbd84b2945b2c361f0f6589bd0
zfh <fuhongzuo@gmail.com>
2020-03-19 23:23:40 +0800

    socket.io房间

--- third-tools/socket.io/server.js
+++ third-tools/socket.io/server.js
@@ -8,12 +8,18 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+  console.log('新用户' + socket.id + '连接');
+
+  // 自定义消息事件（监听客户端消息）
+  socket.on('msg', function(msg){
+    console.log('用户' + socket.id + '消息：' + msg);
+  });
+
+  socket.on('disconnect',()=>{
+    console.log('用户' + socket.id + '断开连接');
   });
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+http.listen(8888, function(){
+  console.log('listening on *:8888');
 });
