5bdefda420053953576a99278af637b845ebb345
zfh <fuhongzuo@gmail.com>
2021-03-13 22:24:46 +0800

    整理笔记

--- example/arraybuffer/main.js
+++ example/arraybuffer/main.js
@@ -6,7 +6,9 @@ const headerLen = 2
 
 // 编码
 const msg = '{"id": 10, "name": "dazuo"}'
+// 返回一个新构造的TextEncoder，它将默认使用utf-8编码生成一个字节流。
 let textEncoder = new TextEncoder()
+// 返回一个包含utf-8编码文本的Uint8Array
 let bodyBuf = textEncoder.encode(msg)
 let headerBuf = new ArrayBuffer(headerLen)
 let headerView = new DataView(headerBuf)
@@ -24,6 +26,7 @@ let data = res.buffer
 let dataView = new DataView(data)
 let bodyLen = dataView.getInt16(0)
 console.log(bodyLen)  // 读取header，获取包体的长度
+// 表示用于特定文本编码的解码器，例如 UTF-8，GBK 等。 默认为 'utf-8' 或 'utf8'，解码器采用字节流作为输入，并发出代码点流。
 let decoder = new TextDecoder()
 let body = decoder.decode(data.slice(headerLen, headerLen + bodyLen))
 console.log(body)
--- syntax/commonjs/utils.js
+++
@@ -1,9 +0,0 @@
-// 1.对外输出
-module.exports.name = "dazuo";
-module.exports.getName = function () {
-    console.log("this method is getName");
-}
-
-// 2.方式二，导出
-// let Utils = "dazuo"
-// module.exports = Utils
